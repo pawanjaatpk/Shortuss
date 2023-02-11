0e3c62d5545f25c5967542231edc71ad30da5747
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-02-11 16:47:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/pawanjaatpk/Shortuss.git /Shortuss
+cd /Shortuss
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
