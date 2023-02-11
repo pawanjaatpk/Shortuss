681a4647d0bb27a1f7934b0685586b53cc25d5ee
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-02-11 17:01:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ API_HASH = os.environ.get("API_HASH", "bd9aed30b22f8b284036cc683d1aa8f4")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5846619148:AAEpGObA7diLDuevrGNX0NQdXzIEnaWi_KA")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5402684050")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://pawanjaatpk:pawanjaatpk@cluster0.espumjg.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5402684050")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(Id Owned Id)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001852972559")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Shortusssite") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
